3b36da6e87817312fc5029440421fdcb4655fecf
Ethan Chapman <ethan.chapman0@outlook.com>
2024-04-05 14:20:54 -0600

    it works!!!

--- src/hdl/TDM4_tb.vhd
+++ src/hdl/TDM4_tb.vhd
@@ -53,29 +53,42 @@ end TDM4_tb;
 architecture test_bench of TDM4_tb is 	
   
 	component TDM4 is
-		-- fill in from TDM4.vhd
-
+		generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+        Port ( i_clk        : in  STD_LOGIC;
+               i_reset        : in  STD_LOGIC; -- asynchronous
+               i_D3         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D2         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D1         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D0         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_data        : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_sel        : out STD_LOGIC_VECTOR (3 downto 0)    -- selected data line (one-cold)
+        );
 	end component TDM4;
 
 	-- Setup test clk (20 ns --> 50 MHz) and other signals
+	constant k_clk_period : time := 20 ns;
 	
 	-- Constants
 	constant k_IO_WIDTH : natural := 4;
-	-- Signals
+	
+	-- Signals    
+    signal w_clk, w_reset: std_logic;
+    signal w_sel: std_logic_vector(3 downto 0);
+    signal w_D0, w_D1, w_D2, w_D3, w_data: std_logic_vector(k_IO_WIDTH - 1 downto 0);
 	
 begin
 	-- PORT MAPS ----------------------------------------
 	-- map ports for any component instances (port mapping is like wiring hardware)
 	uut_inst : TDM4 
-	generic map ( k_WIDTH =>  )
-	port map ( i_clk   => 
-		       i_reset => 
-		       i_D3    => 
-		       i_D2    => 
-		       i_D1    => 
-		       i_D0    => 
-		       o_data  => 
-		       o_sel   => 
+	generic map ( k_WIDTH => k_IO_WIDTH )
+	port map ( i_clk   => w_clk,
+		       i_reset => w_reset,
+		       i_D3    => w_D3,
+		       i_D2    => w_D2,
+		       i_D1    => w_D1,
+		       i_D0    => w_D0,
+		       o_data  => w_data,
+		       o_sel   => w_sel
 	);
 	-----------------------------------------------------	
 	
@@ -83,9 +96,11 @@ begin
 	-- Clock Process ------------------------------------
 	clk_process : process
 	begin
-
-
-
+        w_clk <= '0';
+        wait for k_clk_period/2;
+        
+        w_clk <= '1';
+        wait for k_clk_period/2;
 	end process clk_process;
 	-----------------------------------------------------	
 	
@@ -93,12 +108,35 @@ begin
 	test_process : process 
 	begin
 		-- assign test values to data inputs
-
+        w_D0 <= "0011";
+        w_D1 <= "0110";
+        w_D2 <= "1001";
+        w_D3 <= "1100";
 				
 		-- reset the system first
 		w_reset <= '1';
 		wait for k_clk_period;		
 		w_reset <= '0';
+		
+		assert w_sel = "1110" report "bad thing" severity failure; 
+        assert w_data = "0011" report "bad thing" severity failure; 
+        wait for k_clk_period;   
+                
+        assert w_sel = "1101" report "bad thing" severity failure; 
+        assert w_data = "0110" report "bad thing" severity failure; 
+        wait for k_clk_period;   
+                     
+        assert w_sel = "1011" report "bad thing" severity failure; 
+        assert w_data = "1001" report "bad thing" severity failure; 
+        wait for k_clk_period;   
+                     
+        assert w_sel = "0111" report "bad thing" severity failure; 
+        assert w_data = "1100" report "bad thing" severity failure; 
+        wait for k_clk_period;
+                
+        assert w_sel = "1110" report "bad thing" severity failure; 
+        assert w_data = "0011" report "bad thing" severity failure; 
+        wait for k_clk_period;
 		
 		wait; -- let the TDM do its work
 	end process;	
